ccd051ff0ea8ae513dcbaae00e3bd89588b671e4
joeygaitan <joeygaitan707@gmail.com>
2022-06-16 17:15:10 -0700

    Adding in routes

--- api/schemas/resolvers.js
+++ api/schemas/resolvers.js
@@ -1,14 +1,66 @@
 const db = require("../db/knex");
 const bcrypt = require('bcrypt')
-const { AuthenticationError } = require('apollo-server-express');
+
+const { CreateToken } = require('../utils/authentication')
 
 const resolvers = {
     Query: {
+        GetSelf: async function (parent, args, context) {
+            if (context.user) {
+                try {
+                    const data = await db('user')
+                    .select('username', 'gender', 'bio', 'email', 'gender', 'status', 'age', 'id')
+                    .where('id', context.user.id)
+                    .first()
 
+                    return data;
+                }
+                catch {
+                    throw new AuthenticationError('not logged in');
+                }
+            }
+        },
     },
 
     Mutation: {
+        // TODO: Let user know that they must verify by email to login.
+        SignUp: async (parent, {email, username, password}) => {
+            let body = {email, username, password}
+            // This hashes the newly given password password
+            body.password = await bcrypt.hash(body.password, 10);
+            const user = await db("user")
+            .insert(body)
+            .returning('*')
+            
+            let newUser = user[0]
+            
+            if (newUser)
+            {
+                return ("Please Check your Email for verification");
+            }
 
+            else 
+            {
+                throw new AuthenticationError('Failed to add a new user :/');
+            } 
+        },
+
+        Login: async (parent, { username, password }) => {
+
+            const [user, error] = await tryCatcher(db('user')
+            .select('username', 'gender', 'bio', 'email', 'gender', 'status', 'age', 'password', 'id')
+            .where('username', username)
+            .first(), "failed to find user")
+
+            const [check, error1] = await tryCatcher(bcrypt.compare(password, user.password), "failed to compare passwords")
+
+            if (check)
+            {
+                const token = CreateToken(user)
+                delete user.password
+                return { token, user }
+            }
+        },
     }
 }
 
