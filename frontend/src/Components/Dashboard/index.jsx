cdff4338f034f5affe5c1662f0caec4eafe476dc
joeygaitan <joeygaitan707@gmail.com>
2022-06-30 18:26:54 -0700

    added a lot of styling to the login component

--- frontend/src/Components/Dashboard/index.jsx
+++ frontend/src/Components/Dashboard/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container,Button } from 'semantic-ui-react'
+import { Container, Button } from 'semantic-ui-react'
 
 import { SignOut } from '../../utils/auth'
 
--- frontend/src/Components/Login/index.jsx
+++ frontend/src/Components/Login/index.jsx
@@ -8,7 +8,13 @@ import { LOGIN } from '../../utils/GraphQL/mutations';
 
 import { SetLoginToken } from '../../utils/auth'
 
-import { Container, Button, Input } from 'semantic-ui-react'
+import { Container, Button, Input, Grid, Divider, Header, Icon, Popup } from 'semantic-ui-react'
+
+const style = {
+    borderRadius: 0,
+    opacity: 0.7,
+    padding: '2em',
+  }
 
 const Login = ({props}) => {
 
@@ -91,39 +97,67 @@ const Login = ({props}) => {
 
     return (
         <Container>
-            {(inputState.errorMessage !== "") ? <p>{inputState.errorMessage}</p> : ""}
-            <p>please input username</p>
-            <form onSubmit={onLogin}>
-            <Input icon='search' placeholder='username' content={inputState.username} name='username' onChange={(event)=>{
-                event.preventDefault();
-                const {name, value} = event.target
-                setInputState({
-                  ...inputState,
-                  [name]:value  
-                })
-            }}/>
-            <br/>
-            <p>please input password</p>
-            <Input icon='search' placeholder='username' content={inputState.password} type={(inputState.displayPassword) ? "text":"password"} name='password' onChange={(event)=>{
-                event.preventDefault();
-                const {name, value} = event.target
-                setInputState({
-                  ...inputState,
-                  [name]:value  
-                })
-            }}/>
-            <Button content="display password" onClick={(event)=>{
-                event.preventDefault();
-                setInputState({
-                    ...inputState,
-                    ["displayPassword"]:!inputState.displayPassword
-                })
-            }}/>
-            <br/>
-            <Button type="submit" content="Login"/>
-            <br/>
-            </form>
-            <Link to="/signup"><Button content="New Here? Sign up!"/></Link>
+            <Grid verticalAlign='middle' padded="vertically" centered  columns={2}>
+                <Grid.Row>
+                    <Grid.Column centered verticalAlign='middle' padded="vertically">
+                        <Header as='h1'>
+                        <Icon name='dna' />
+                        <Header.Content>User Login</Header.Content>
+                        </Header>
+                            {(inputState.errorMessage !== "") ? <p>{inputState.errorMessage}</p> : ""}
+                    </Grid.Column>
+                </Grid.Row>
+
+                <Grid.Column verticalAlign="middle" centered>
+                    <Grid verticalAlign='middle' padded="vertically" centered  columns={1} rows={2}>
+                        <form onSubmit={onLogin}>
+                        <Grid.Row>
+                            <Input icon='user' placeholder='username' content={inputState.username} name='username' onChange={(event)=>{
+                                event.preventDefault();
+                                const {name, value} = event.target
+                                setInputState({
+                                ...inputState,
+                                [name]:value  
+                                })
+                            }}/>
+                        </Grid.Row>
+                        <Divider horizontal></Divider>
+                        <Grid.Row columns={2}>
+                            <Grid.Column>
+                                <Input icon='lock' placeholder='password' content={inputState.password} type={(inputState.displayPassword) ? "text":"password"} name='password' onChange={(event)=>{
+                                    event.preventDefault();
+                                    const {name, value} = event.target
+                                    setInputState({
+                                    ...inputState,
+                                    [name]:value  
+                                    })
+                                }}/>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Popup
+                                    trigger={<Button icon="eye" onClick={(event)=>{
+                                        event.preventDefault();
+                                        setInputState({
+                                            ...inputState,
+                                            ["displayPassword"]:!inputState.displayPassword
+                                        })
+                                    }}/>}
+                                    content='Display Password'
+                                    style={style}
+                                    inverted
+                                />
+                            </Grid.Column>   
+                        </Grid.Row>
+                        <Divider horizontal></Divider>
+                        <Grid.Row>
+                            <Button type="submit" content="Login"/>
+                        </Grid.Row>
+                        </form>
+                    </Grid>
+                    <Divider horizontal>Or</Divider>
+                    <Link to="/signup"><Button content="New Here? Sign up!"/></Link>
+                </Grid.Column>
+            </Grid>
         </Container>
     )
 }
