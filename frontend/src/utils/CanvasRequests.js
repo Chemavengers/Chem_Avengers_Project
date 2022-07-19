ffee034dee3164a651a8f2fc3d5e2209bbfcedde
joeygaitan <joeygaitan707@gmail.com>
2022-07-18 20:52:59 -0700

    adding in more work with the api

--- frontend/src/utils/CanvasRequests.js
+++ frontend/src/utils/CanvasRequests.js
@@ -1,19 +1,28 @@
 const GetModules = async () => {
-    console.log(process.env.REACT_APP_CANVAS_API_KEY)
     let modules;
 
     try {
-        let apiCall = await fetch(`${process.env.REACT_APP_CANVAS_URL}/modules/`, {
-            method: 'get',
-            mode: 'cors',
+        console.log((process.env.REACT_APP_CANVAS_API_KEY))
+
+        let string = process.env.REACT_APP_CANVAS_API_KEY ? `Bearer ${process.env.REACT_APP_CANVAS_API_KEY}`: ''
+
+        let apiCall = await fetch(`${process.env.REACT_APP_CANVAS_URL}modules`, {
+            'method': 'GET',
+            'mode': "cors",
+            'json': true,
+            'resolveWithFullResponse': true,
             headers : {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': process.env.REACT_APP_CANVAS_API_KEY ? `Bearer ${process.env.REACT_APP_CANVAS_API_KEY}`: ''   
+                'Authorization': string,   
+                // 'Content-Type': '*/*',
+                // 'Accept': 'application/json',
+                // "Connection": "keep-alive",
             }
         })
 
+        console.log(apiCall)
+        
         modules = apiCall.json()
+        
     }
     catch {
         console.log("Failed to call the api :(")
